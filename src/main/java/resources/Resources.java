0ad55be1ae364875d34fb7ba0469f4318e12001a
SergeySementsov <sersementsov@gmail.com>
2015-04-29 00:46:01 +0400

    monstrous bicycle commit

---
+++ src/main/java/resources/Resources.java
@@ -0,0 +1,10 @@
+package resources;
+
+public final class Resources {
+    // TODO: ask if I can do this as in Android string.xml resources file
+    public static final String STUDENT_JSON_FILE = "student.json";
+    public static final String TEACHER_JSON_FILE = "teacher.json";
+    public static final String SUBJECT_JSON_FILE = "subject.json";
+    public static final String ROOM_JSON_FILE    = "room.json";
+    public static final String GROUP_JSON_FILE   = "group.json";
+}
